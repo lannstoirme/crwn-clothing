a3e56800c96baff36cecf59e55072a1b00605f12
Veronica Woodward (lannstoirme on Discord) <46139638+lannstoirme@users.noreply.github.com>
2021-06-08 14:46:57 +1000

    Started app with Create React App & added homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Components
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
